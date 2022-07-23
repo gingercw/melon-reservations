259c26afced9326b09fcdd86446c77f21c0e92cb
gingercw <gingercw@gmail.com>
2022-07-23 15:50:53 +0000

    center text on screen

--- server.py
+++ server.py
@@ -119,4 +119,4 @@ def show_user_reservations(user_id):
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
     connect_to_db(app)
-    app.run(host="0.0.0.0", debug=True)
+    # app.run(host="0.0.0.0", debug=True)
